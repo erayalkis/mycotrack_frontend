b3308e7f0a963aeebb779c763d440b6bf0a2653d
Eray Alkış <lolpo12345@outlook.com>
2023-03-28 23:58:33 +0300

    Create base alert component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
   <main class="w-full h-full">
     <RouterView />
   </main>
+  <AlertMain message="Debug Message." type="warn" />
 </template>
 <script setup lang="ts">
+import AlertMain from './components/Alert/AlertMain.vue'
 import NavbarMain from './components/Navbar/NavbarMain.vue'
 </script>
---
+++ src/components/Alert/AlertMain.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-36 absolute bg-red-300 rounded-md border border-gray-100 bottom-0 right-36">
+    <div class="flex justify-between">
+      <img :src="checkCircle" v-if="type === 'success'" />
+      <img :src="exclamationCirlce" v-if="type === 'warn'" />
+      <img :src="infoCircle" v-if="type === 'info'" />
+      <img :src="exclamationTriangle" v-if="type === 'error'" />
+      <div>{{ message }}</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import checkCircle from '@/assets/svg/check-circle.svg'
+import exclamationCirlce from '@/assets/svg/exclamation-circle.svg'
+import infoCircle from '@/assets/svg/info-circle.svg'
+import exclamationTriangle from '@/assets/svg/exclamation-triangle.svg'
+
+defineProps({
+  message: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  }
+})
+</script>
