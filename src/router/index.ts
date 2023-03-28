e1e0806eefb8201450aeea56f7cadd2b1dd2e79e
Eray Alkış <lolpo12345@outlook.com>
2023-03-28 04:00:26 +0300

    Block users from navigating to home if not logged in

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import LoginVİewVue from '@/views/LoginVİew.vue'
 import SignupViewVue from '@/views/SignupView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      beforeEnter: (from, to, next) => {
+        const { isLoggedIn } = useUserStore()
+        if (isLoggedIn) {
+          next()
+        } else {
+          next('/login')
+        }
+      }
     },
     {
       path: '/login',
