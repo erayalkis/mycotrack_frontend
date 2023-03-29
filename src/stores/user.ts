1511d7cf207935584334ec34fd12f3db9bd98ba7
Eray Alkış <lolpo12345@outlook.com>
2023-03-30 01:13:48 +0300

    Create function for signing up

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,6 +1,7 @@
 import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 import { serverConfig } from '@/config/serverConfig'
+import { useAlertStore } from './alert'
 
 type User = {
   username: String | null
@@ -8,6 +9,8 @@ type User = {
 }
 
 export const useUserStore = defineStore('user', () => {
+  const { addMessage } = useAlertStore()
+
   const isLoggedIn: Ref<boolean> = ref(false)
   const token: Ref<String> = ref('')
   const data: Ref<User> = ref({
@@ -31,19 +34,59 @@ export const useUserStore = defineStore('user', () => {
       body: JSON.stringify(data)
     })
 
+    const json = await res.json()
+    json['status'] = res.status
+
     if (res.status !== 200) {
       console.error('Error while logging in!')
       console.log(res)
+      addMessage({ content: json.error, type: 'error' })
+      return json
     }
 
     const resToken = res.headers.get('authorization')
-    const json = await res.json()
-
     if (resToken) token.value = resToken
+
+    addMessage({ content: json.message, type: 'success' })
     return json
   }
 
-  const signUp = () => {}
+  const signUp = async (
+    username: String,
+    email: String,
+    password: String,
+    passwordConfirmation: String
+  ) => {
+    const data = {
+      user: {
+        username,
+        email,
+        password,
+        password_confirmation: passwordConfirmation
+      }
+    }
+
+    const res = await fetch(`${serverConfig.serverUrl}/users/sign_up`, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+
+    const json = await res.json()
+    json['status'] = res.status
+
+    if (res.status !== 200) {
+      console.error('Error while signing up!')
+      console.log(res)
+      addMessage({ content: json.error, type: 'error' })
+      return json
+    }
+
+    const resToken = res.headers.get('authorization')
+    if (resToken) token.value = resToken
+
+    addMessage({ content: json.message, type: 'success' })
+    return json
+  }
 
   const signOut = () => {}
 
