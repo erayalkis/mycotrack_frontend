932fce8ddfa8e02269544917e4c3150866b200fb
Eray Alkış <lolpo12345@outlook.com>
2023-03-30 23:38:47 +0300

    Load user token on startup

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
   <AlertMain message="Debug Message." type="success" />
 </template>
 <script setup lang="ts">
+import { onBeforeMount } from 'vue'
 import AlertMain from './components/Alert/AlertMain.vue'
 import NavbarMain from './components/Navbar/NavbarMain.vue'
+import { useUserStore } from './stores/user'
+
+const { loadToken } = useUserStore()
+onBeforeMount(async () => {
+  await loadToken()
+})
 </script>
