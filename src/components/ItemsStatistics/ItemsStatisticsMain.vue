1889c847cc4c542d405b4ac63d73f401f1e5e125
Eray Alkış <erayalkis@hotmail.com>
2023-04-11 04:03:34 +0300

    Add template for graphs

--- src/components/ItemsStatistics/ItemsStatisticsMain.vue
+++ src/components/ItemsStatistics/ItemsStatisticsMain.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="p-2 text-center flex flex-col justify-center mt-2 items-center mt-10">
     <h1 class="text-2xl mb-3">Statistics</h1>
-    <div class="flex gap-5 flex-wrap items-center justify-center h-full w-full"></div>
+    <div class="flex gap-5 flex-wrap items-center justify-center h-full w-full">
+      <div class="border border-gray-300 rounded-md w-full h-80 select-none md:w-80">
+        <h1>Number of times culture used</h1>
+        <div>Graph</div>
+      </div>
+      <div class="border border-gray-300 rounded-md w-full h-80 select-none md:w-80">
+        <h1>Number of times spawn used</h1>
+        <div>Graph</div>
+      </div>
+      <div class="border border-gray-300 rounded-md w-full h-80 select-none md:w-80">
+        <h1>Status of blocks</h1>
+        <div>Graph</div>
+      </div>
+    </div>
   </div>
 </template>
