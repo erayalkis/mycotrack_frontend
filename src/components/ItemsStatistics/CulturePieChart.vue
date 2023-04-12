db913676d173f7625046dd256a44653e1600bbb9
Eray Alkış <erayalkis@hotmail.com>
2023-04-13 01:22:39 +0300

    Improve label rendering

--- src/components/ItemsStatistics/CulturePieChart.vue
+++ src/components/ItemsStatistics/CulturePieChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <DoughnutChart ref="doughnutRef" :chartData="testData" :options="options" />
+    <DoughnutChart ref="doughnutRef" :chartData="chartData" :options="options" />
   </div>
 </template>
 
@@ -8,35 +8,90 @@
 import { computed, defineComponent, ref } from 'vue'
 import { DoughnutChart, type ExtractComponentData } from 'vue-chart-3'
 import { Chart, registerables } from 'chart.js'
+import { useStatisticsStore } from '@/stores/statistics'
+import { mapStores } from 'pinia'
 
 Chart.register(...registerables)
+
 export default defineComponent({
   name: 'CultureUsageDonutChart',
   components: { DoughnutChart },
+
   setup() {
-    const data = ref([30, 40, 60, 70, 5])
     const doughnutRef = ref<ExtractComponentData<typeof DoughnutChart>>()
 
-    const options = {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false
+    return { doughnutRef }
+  },
+  computed: {
+    ...mapStores(useStatisticsStore),
+    chartData() {
+      return {
+        labels: ['Unused', 'Used Once', 'Used Multiple Times'],
+        datasets: [
+          {
+            data: [
+              this.statisticsStore.cultureUsageStatistics.unusedCultures.length,
+              this.statisticsStore.cultureUsageStatistics.usedOnceCultures.length,
+              this.statisticsStore.cultureUsageStatistics.usedMultipleCultures.length
+            ],
+            backgroundColor: '#77CEFF'
+          }
+        ]
+      }
+    },
+    options() {
+      return {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              afterBody: (item: any) => {
+                let body = '\n'
+                if (item[0].label === 'Unused') {
+                  this.statisticsStore.cultureUsageStatistics.unusedCultures.forEach(
+                    (culture, idx) => {
+                      const newLine = `- ${culture.genus + ' ' + culture.species}`
+                      const isLastIdx =
+                        idx ===
+                        this.statisticsStore.cultureUsageStatistics.unusedCultures.length - 1
+                      body += newLine
+                      if (!isLastIdx) body += '\n'
+                    }
+                  )
+                } else if (item[0].label === 'Used Once') {
+                  this.statisticsStore.cultureUsageStatistics.usedOnceCultures.forEach(
+                    (culture, idx) => {
+                      const newLine = `- ${culture.genus + ' ' + culture.species}`
+                      const isLastIdx =
+                        idx ===
+                        this.statisticsStore.cultureUsageStatistics.usedOnceCultures.length - 1
+                      body += newLine
+                      if (!isLastIdx) body += '\n'
+                    }
+                  )
+                } else if (item[0].label === 'Used Multiple Times') {
+                  this.statisticsStore.cultureUsageStatistics.usedMultipleCultures.forEach(
+                    (culture, idx) => {
+                      const newLine = `- ${culture.genus + ' ' + culture.species}`
+                      const isLastIdx =
+                        idx ===
+                        this.statisticsStore.cultureUsageStatistics.usedMultipleCultures.length - 1
+                      body += newLine
+                      if (!isLastIdx) body += '\n'
+                    }
+                  )
+                }
+
+                return body
+              }
+            }
+          }
         }
       }
     }
-
-    const testData = computed(() => ({
-      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
-      datasets: [
-        {
-          data: data.value,
-          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED']
-        }
-      ]
-    }))
-
-    return { testData, doughnutRef, options }
   }
 })
 </script>
