8a4beba6bf27634ce372994d89faa50e4c99fd5d
Eray Alkış <erayalkis@hotmail.com>
2023-04-19 03:56:46 +0300

    Update stats styling

--- src/components/ItemsDisplay/ItemsDisplayMain.vue
+++ src/components/ItemsDisplay/ItemsDisplayMain.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-2 text-center flex flex-col justify-center mt-2 items-center">
-    <div class="flex gap-2 items-center">
-      <h1 class="text-2xl mb-3">Quick Access</h1>
-      <MenuSvg class="w-8 h-8 text-blue-500 mb-2.5" />
+    <div class="flex gap-2 items-center mb-5 mt-1">
+      <h1 class="text-2xl">Quick Access</h1>
+      <MenuSvg class="w-8 h-8 text-blue-500 mt-0.5" />
     </div>
 
     <div class="flex gap-5 flex-wrap items-center justify-center h-full w-full">
