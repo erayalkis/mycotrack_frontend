349ef6812b9c3afda472b53c86365908ea066606
Eray Alkış <lolpo12345@outlook.com>
2023-04-21 05:11:13 +0300

    Update serverConfig production URL

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -2,5 +2,5 @@ export const serverConfig = {
   serverUrl:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://sitethatdoesntexistyet.com'
+      : 'https://mycotrack.onrender.com'
 }
